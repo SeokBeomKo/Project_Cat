005c6dc1a4815f03cbb6f05c0420b781c6a85c55
ChoMinkyung <mkjo99@naver.com>
2023-10-30 15:38:36 +0900

    Feat : 캡슐, 스위치 기능 추가

--- Assets/Scripts/Object/SwitchOperation.cs
+++ Assets/Scripts/Object/SwitchOperation.cs
@@ -5,129 +5,103 @@ using UnityEngine;
 
 public class SwitchOperation : MonoBehaviour
 {
-    [Header("힘이 가해졌을 때 색이 변할 Mesh")]
-    public MeshRenderer Mesh;
-    Material mat;
+    public GameObject button; // 버튼 오브젝트
+    public float downSpeed = 1.0f; // 버튼 내리는 속도
+    public float targetY = 0.3f; // 버튼이 도달할 Y 좌표
+    public float threshold = 1.0f; // 힘의 임계값
 
-    [Header("힘이 가해질 대상")]
-    public GameObject Button;
+    public Color32 activatedColor = new Color32(255, 102, 102, 255); // 활성화된 상태의 색
 
-    private bool press = false;
-    private bool up = false;
-    private bool isActivated = false;
+    private Rigidbody buttonRigidbody;
+    private float collisionForce;
 
-    private Vector3 buttonOriginPosition;
-
-    [Header("힘을 감지할 임계치")]
-    public float ForceThreshold = 10.0f;
-    float collisionForce;
-
-    [Header("대상 이동 속도")]
-    public float Speed = 5.0f; // 이동 속도 설정
-
-    Collision collisionObject;
-
-    [Header("스위치 작동시 움직일 대상")]
-    public GameObject Door;
-    public float DoorSpeed = 2;
-
-    private bool isClosed = false;
-
+    private bool isActivated = false; // 버튼 활성화 상태 여부
+    private Vector3 initialPosition; // 초기 위치 저장
+    private bool isButtonMoving = false; // 버튼 움직이는 중 여부
+    
     private void Start()
     {
-        buttonOriginPosition = Button.transform.position;
-        mat = Mesh.material;
+        initialPosition = button.transform.position;
     }
 
-    void Update()
+    private void Update()
     {
-        if (!isActivated && press)
+        if (isActivated)
         {
-            PressSwitch();
+            button.GetComponent<Renderer>().material.color = activatedColor;
         }
-
-        if (!isActivated && up)
-        {
-            UpSwitch();
-        }
-
-        if(isActivated && !isClosed)
-        {
-            CloseDoor();
-        }
-
-        if(isActivated)
-        {
-            mat.color = new Color32(255, 102, 102, 255);
-        }
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        collisionObject = collision;
-        Rigidbody collisionRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-        float collisionMass = collisionRigidbody.mass;
-        float gravity = 9.8f;
 
-        collisionForce = collision.relativeVelocity.magnitude + collisionMass * gravity;
-
-        if ((collisionForce >= ForceThreshold) && !isActivated)
+        if(collision.transform.CompareTag(""))
         {
-            press = true;
-        }
 
+        }
+        StartCoroutine(PressButton());
+        // 충돌한 객체의 힘을 가져오고, 그 힘이 threshold보다 큰지 확인
+        //collisionForce = collision.relativeVelocity.magnitude;// + (Physics.gravity.magnitude * collision.rigidbody.mass);
+        //if (collisionForce >= threshold && !isButtonMoving)
+        //{
+        //}
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        up = true;
-        press = false;
-        mat.color = new Color32(255, 0, 0, 255);
-        collisionObject = null;
-    }
-
-    private void CloseDoor()
-    {
-        Door.transform.Translate(Vector3.down * DoorSpeed * Time.deltaTime);
-        if (Door.transform.position.y < 3)
+        // 충돌한 객체가 사라졌을 때
+        if (!isButtonMoving)
         {
-            isClosed = true;
+            StartCoroutine(ReleaseButton());
         }
     }
 
-    private void PressSwitch()
+    private IEnumerator PressButton()
     {
-        Button.transform.Translate(Vector3.down * collisionForce * Time.deltaTime);
-        collisionObject.transform.Translate(Vector3.down * collisionForce * Time.deltaTime);
+        isButtonMoving = true;
 
-        if (Button.transform.position.y < 0.3)
+        while (!isActivated && button.transform.position.y > targetY)
         {
-            Button.transform.Translate(Button.transform.position.x, 0.4f, Button.transform.position.z);
-            press = false;
+            button.transform.Translate(Vector3.down * downSpeed * Time.deltaTime);
+            transform.Translate(Vector3.down * downSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        if (!isActivated)
+        {
             isActivated = true;
         }
-        transform.position = Button.transform.position;
 
-        mat.color = new Color32(255, 102, 102, 255);
+        isButtonMoving = false;
     }
 
-    private void UpSwitch()
+    private IEnumerator ReleaseButton()
     {
-        if (Button.transform.position.y >= buttonOriginPosition.y)
-        {
-            up = false;
-            transform.position = Button.transform.position;
-        }
-        else
-        {
-            Button.transform.Translate(Vector3.up * Speed * Time.deltaTime);
-            if (collisionObject != null)
-            {
+        isButtonMoving = true;
 
-                collisionObject.transform.Translate(Vector3.up * Speed * Time.deltaTime);
-            }
+        while (button.transform.position.y < initialPosition.y)
+        {
+            button.transform.Translate(Vector3.up * downSpeed * Time.deltaTime);
+            transform.Translate(Vector3.up * downSpeed * Time.deltaTime);
+
+            yield return null;
         }
+
+        isButtonMoving = false;
     }
+
+    public bool GetSwitchState()
+    {
+        return isActivated;
+    }
+
+    //public void SetSwitchPress(bool press)
+    //{
+    //    this.press = press;
+    //    if (press)
+    //    {
+    //        collisionForce = 10;
+    //    }
+    //}
 
 }
