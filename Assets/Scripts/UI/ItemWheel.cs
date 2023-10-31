ccb85af0bf1c39093c9c0a7af0ec2245f2eef615
mangruf <pinky0927@naver.com>
2023-10-31 14:21:00 +0900

    Update : 운동 에너지 팝업창 UI
    20231031 한결

--- Assets/Scripts/UI/ItemWheel.cs
+++ Assets/Scripts/UI/ItemWheel.cs
@@ -8,7 +8,7 @@ public class ItemWheel : MonoBehaviour
     public Transform center; // 중앙을 기준으로 마우스 각도 계산
     public Transform selectObject; // 선택된 거 회전
 
-    public GameObject itemMenu;
+    public GameObject itemMenu; // 아이템 휠 메뉴
     bool isActive; // 메뉴의 활성 상태
 
     public TextMeshProUGUI itemName; // 아이템 이름
@@ -18,8 +18,10 @@ public class ItemWheel : MonoBehaviour
     public string[] itemExplanationArray;
 
     public Transform[] itemSlotArray; // 아이템 이미지 확대
+    public Transform[] energySlotArray; // 운동에너지 이미지 확대
 
-    public Transform min, max; // 원 경계
+    public Transform itemMin, itemMax; // 아이템 휠 원 경계
+    public Transform energyMin, energyMax; // 운동에너지 원 경계
 
     public GameObject kineticEnergyMenu; // 운동 에너지 선택시 메뉴창
     bool isMenuActive;
@@ -40,16 +42,16 @@ public class ItemWheel : MonoBehaviour
         if (Input.GetMouseButtonDown(2)) // 마우스 휠 버튼 무기창 활성화
         {
             isActive = !isActive;
-            if (isActive)
+            if (isActive && !isMenuActive)
                 itemMenu.SetActive(true);
-            else
+            if(!isActive)
                 itemMenu.SetActive(false);
         }
 
         if (isActive)
         {
             // 중앙으로부터의 마우스 거리가 경계값 안에 있는지 확인
-            if (Vector3.Distance(Input.mousePosition, center.position) < Vector3.Distance(max.position, center.position) && Vector3.Distance(Input.mousePosition, center.position) > Vector3.Distance(min.position, center.position))
+            if (Vector3.Distance(Input.mousePosition, center.position) < Vector3.Distance(itemMax.position, center.position) && Vector3.Distance(Input.mousePosition, center.position) > Vector3.Distance(itemMin.position, center.position))
             {
                 // 각도 계산 
                 Vector2 delta = center.position - Input.mousePosition; // 중앙에서부터 마우스 위치의 차이
@@ -60,7 +62,7 @@ public class ItemWheel : MonoBehaviour
 
                 for (int i = 0; i < 360; i += 60)
                 {
-                    if (angle >= i + 30 && angle < i + 90)
+                    if (angle >= i && angle < i + 60)
                     {
                         selectObject.eulerAngles = new Vector3(0, 0, i); // Z축 주위로 회전
 
@@ -75,11 +77,18 @@ public class ItemWheel : MonoBehaviour
                         itemSlotArray[currentItem].transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
 
                         if (Input.GetMouseButtonDown(0))
-                        {
+                        { 
                             Debug.Log(angle);
                             Debug.Log(itemNameArray[currentItem] + "선택");
                             isActive = false;
                             itemMenu.SetActive(false);
+
+                            // 운동에너지 선택시 새로운 팝업창 생성
+                            if(angle >= 300 && angle < 360)
+                            {
+                                isMenuActive = true;
+                                kineticEnergyMenu.SetActive(true);
+                            }
                         }
                     }
                     currentItem++;
@@ -112,6 +121,60 @@ public class ItemWheel : MonoBehaviour
                 itemMenu.SetActive(false);
             }
         }
+
+        if (isMenuActive)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log("운동 에너지 선택 취소");
+                isMenuActive = false;
+                kineticEnergyMenu.SetActive(false);
+
+                isActive = true;
+                itemMenu.SetActive(true);
+            }
+            else if (Input.GetMouseButtonDown(2))
+            {
+                Debug.Log("선택 취소");
+                isMenuActive = false;
+                kineticEnergyMenu.SetActive(false);
+            }
+
+            if (Vector3.Distance(Input.mousePosition, center.position) < Vector3.Distance(energyMax.position, center.position) && Vector3.Distance(Input.mousePosition, center.position) > Vector3.Distance(energyMin.position, center.position))
+            {
+                // 각도 계산 
+                Vector2 delta2 = center.position - Input.mousePosition; // 중앙에서부터 마우스 위치의 차이
+                float angle2 = Mathf.Atan2(delta2.y, delta2.x) * Mathf.Rad2Deg; // 각도 구하는 공식 
+                angle2 += 180;
+
+                if (angle2 > 90 && angle2 < 270)
+                {
+                    energySlotArray[0].transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+                    energySlotArray[1].transform.localScale = new Vector3(1, 1, 1);
+                }
+                else
+                {
+                    energySlotArray[0].transform.localScale = new Vector3(1, 1, 1);
+                    energySlotArray[1].transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+                }
+                        /*if (Input.GetMouseButtonDown(0))
+                        {
+                            Debug.Log(energySlotArray[currentItem2] + "선택");
+                            isMenuActive = false;
+                            kineticEnergyMenu.SetActive(false);
+                        }*/
+                
+            }
+            else
+            {
+                foreach (Transform e in energySlotArray)
+                {
+                    e.transform.localScale = new Vector3(1, 1, 1);
+                }
+            }
+
+        }
+
     }
 }
 
