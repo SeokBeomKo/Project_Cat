3184cfb84239c84459ecdff4e358ec81f1003916
mangruf <pinky0927@naver.com>
2023-11-06 15:26:30 +0900

    Feat : 게임 로비씬
    한결

---
+++ Assets/Scripts/Weapon/ObjectPoolManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : Singleton<ObjectPoolManager>
+{
+
+
+
+    // 풀 생성
+    // 풀 
+    // 풀로 반환
+}
