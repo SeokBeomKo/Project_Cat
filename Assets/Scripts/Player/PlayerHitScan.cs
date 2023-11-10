1abec00e08307e7eb9b1fa9112ea5211c1683e80
SeokBeomKo <enngo98@gmail.com>
2023-11-10 16:26:17 +0900

    Update : 플레이어 무적 시간

--- Assets/Scripts/Player/PlayerHitScan.cs
+++ Assets/Scripts/Player/PlayerHitScan.cs
@@ -12,6 +12,11 @@ public class PlayerHitScan : MonoBehaviour
     [SerializeField]
     private Collider lowerCollider;
 
+    [SerializeField]
+    private float invincibleTime = 0.5f; // 무적 시간 설정. 단위는 초입니다.
+    
+    private float lastHitTime; // 마지막으로 공격을 당한 시간을 저장합니다.
+
     public void EnterJumpPlayer()
     {
         lowerCollider.enabled = false;
@@ -24,8 +29,9 @@ public class PlayerHitScan : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) 
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("EnemyAttack"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("EnemyAttack") && Time.time - lastHitTime > invincibleTime)
         {
+            lastHitTime = Time.time; // 공격을 당한 시간을 갱신합니다.
             OnPlayerHitScan?.Invoke(5);
         }
     }
