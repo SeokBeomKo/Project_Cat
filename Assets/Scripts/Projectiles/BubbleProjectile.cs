a27442af992e8635fa4e2b349ff00084c7e46cd9
SeokBeomKo <enngo98@gmail.com>
2023-11-14 12:29:21 +0900

    Update : 비눗방울 발사체 로직 수정

--- Assets/Scripts/Projectiles/BubbleProjectile.cs
+++ Assets/Scripts/Projectiles/BubbleProjectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BubbleProjectile : MonoBehaviour
@@ -7,18 +8,40 @@ public class BubbleProjectile : MonoBehaviour
     public Rigidbody rigidBody;
     public Vector3 directionPosition;
 
+    public float maxSpeed  = 0f;
+
+    public bool isBounce;
+
     private void Start() 
     {
-        rigidBody.AddForce(Vector3.up, ForceMode.Force);
+        // 수직 방향으로 힘을 가함
+        
     }
 
     public void SetDirection(Vector3 direction)
     {
         directionPosition = direction;
+        rigidBody.AddForce(Vector3.up * 200 + directionPosition * 100, ForceMode.Force);
+    }
+
+    public void UseGravity()
+    {
+        rigidBody.AddForce(Vector3.down * 0.5f, ForceMode.Force);
     }
 
     private void Update() 
     {
-        rigidBody.velocity = directionPosition * 1f + (Vector3.down * 0.1f);
+        UseGravity();
+        Vector3 flatVel = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
+
+        if (flatVel.magnitude > maxSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * maxSpeed;
+            rigidBody.velocity = new Vector3(limitedVel.x, rigidBody.velocity.y, limitedVel.z);
+        }
+        else
+        {
+            rigidBody.AddForce(directionPosition * 5f, ForceMode.Force);
+        }
     }
 }
