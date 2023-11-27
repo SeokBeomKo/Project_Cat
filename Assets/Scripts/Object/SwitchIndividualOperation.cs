95664727ac24d372c05832af7eefc7ec8483b715
ChoMinkyung <mkjo99@naver.com>
2023-11-27 16:30:11 +0900

    Feat : Room2_Lab 1차 완성,  Room1_Bed 작업 중

--- Assets/Scripts/Object/SwitchIndividualOperation.cs
+++ Assets/Scripts/Object/SwitchIndividualOperation.cs
@@ -2,52 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchIndividualOperation : SwitchesOperation
+public class SwitchIndividualOperation : MonoBehaviour
 {
+    public SwitchesOperation switchesOperation;
+
     public int IndividualIndex;
     public GameObject Switchbone;
     private bool isSwitchOn = false;
+    private float angle;
+
+    public Room2GameCenter room2GameCenter;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerAttack"))
+        if (room2GameCenter.IsSwitchesUnlocked)
         {
-            this[IndividualIndex] = !this[IndividualIndex];
-            isSwitchOn = !isSwitchOn;
-
-            // on : -40 , off : 40
-            if (isSwitchOn)
+            if (other.gameObject.layer == LayerMask.NameToLayer("PlayerAttack"))
             {
-                Switchbone.transform.rotation = Quaternion.Euler(0, 40, 0);
-            }
-            else
-            {
-                Switchbone.transform.rotation = Quaternion.Euler(0, -40, 0);
+
+                switchesOperation[IndividualIndex, true] = !switchesOperation[IndividualIndex, true];
+                isSwitchOn = !isSwitchOn;
+
+                // on : -40 , off : 40
+                if (isSwitchOn)
+                {
+                    angle = -50;
+                }
+                else
+                {
+                    angle = -130;
+                }
+                Debug.Log(angle);
+                Switchbone.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                Debug.Log(Switchbone.transform.rotation.x);
 
             }
-
         }
     }
 
     // Test 
-    void Update()
-    {
-        // 키패드에서 1번 키를 입력받으면 OnKeyPressed 메서드 호출
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            this[IndividualIndex] = !this[IndividualIndex];
-            isSwitchOn = !isSwitchOn;
+    //void Update()
+    //{
+    //    // 키패드에서 1번 키를 입력받으면 OnKeyPressed 메서드 호출
+    //    if (Input.GetKeyDown(KeyCode.Alpha1))
+    //    {
+    //        switchesOperation[IndividualIndex] = !switchesOperation[IndividualIndex];
+    //        isSwitchOn = !isSwitchOn;
 
-            // on : -40 , off : 40
-            if (isSwitchOn)
-            {
-                Switchbone.transform.rotation = Quaternion.Euler(0, 40, 0);
-            }
-            else
-            {
-                Switchbone.transform.rotation = Quaternion.Euler(0, -40, 0);
-
-            }
-        }
-    }
+    //        // on : -40 , off : 40
+    //        if (isSwitchOn)
+    //        {
+    //            angle = -50;
+    //        }
+    //        else
+    //        {
+    //            angle = -130;
+    //        }
+    //        Debug.Log(angle);
+    //        Switchbone.transform.rotation = Quaternion.Euler(angle, 0, 0);
+    //        Debug.Log(Switchbone.transform.rotation.x);
+    //    }
+    //}
 }
