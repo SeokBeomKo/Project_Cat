4ff039d20dd0abae0e84cbd1eac00dc9ce787772
SeokBeomKo <enngo98@gmail.com>
2023-11-28 20:07:40 +0900

    Feat : 바이러스 폭발 이펙트 추가

--- Assets/Scripts/Object/VirusStaticOperation.cs
+++ Assets/Scripts/Object/VirusStaticOperation.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class VirusStaticOperation : MonoBehaviour
 {
+    // TODO : 옵저버 패턴 .
     public float HP = 5;
     public ObjectHPbar objectHPbar;
+
+    [Header("모델링 정보")]
+    public GameObject model;
+    public GameObject explosionVFX;
 
     void Start()
     {
@@ -19,6 +24,7 @@ public class VirusStaticOperation : MonoBehaviour
         {
             objectHPbar.Demage(1);
             HP = objectHPbar.GetHP();
+            Check();
         } 
     }
 
@@ -27,6 +33,7 @@ public class VirusStaticOperation : MonoBehaviour
         if (collision.gameObject.CompareTag("Ball"))
         {
             HP = 0;
+            Check();
         }
         else if (collision.gameObject.CompareTag("Player"))
         {
@@ -34,11 +41,26 @@ public class VirusStaticOperation : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void Check()
     {
         if (HP == 0)
         {
-            transform.parent.gameObject.SetActive(false);
+            model.SetActive(false);
+            explosionVFX.SetActive(true);
+
+            StartCoroutine(DestroyAfterParticles());
         }
+    }
+
+    private IEnumerator DestroyAfterParticles()
+    {
+        ParticleSystem ps = explosionVFX.GetComponent<ParticleSystem>();
+    
+        while (ps != null && ps.IsAlive())
+        {
+            yield return null;
+        }
+    
+        Destroy(transform.parent.gameObject);
     }
 }
