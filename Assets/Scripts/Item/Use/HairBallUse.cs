955e039c175d08f69e9ffa36691a2ca7fa826556
mangruf <pinky0927@naver.com>
2023-12-02 17:51:02 +0900

    Update : 아이템 - UI 연결

--- Assets/Scripts/Item/Use/HairBallUse.cs
+++ Assets/Scripts/Item/Use/HairBallUse.cs
@@ -5,48 +5,30 @@ using UnityEngine;
 
 public class HairBallUse : MonoBehaviour
 {
-    public GameObject Player;
     public GameObject HairBall;
     public float obstacleCheckDistance = 1; // 장애물을 확인할 거리
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            Debug.Log("Input 9");
-
-            if (!CheckObstacleInFront())
-            {
-                CreateHairBall();
-            }
-            else
-            {
-                Debug.Log("장애물이 있어 생성할 수 없음");
-            }
-        }
-    }
-
-    void CreateHairBall()
+    public void CreateHairBall(Vector3 position, Vector3 forward)
     {
         Debug.Log("CreateHairBall");
 
         // 플레이어의 위치를 얻습니다.
-        Vector3 playerPosition = Player.transform.position;
+        Vector3 playerPosition = position;
 
         // 플레이어 앞쪽에 위치를 계산합니다.
-        Vector3 spawnPosition = playerPosition + Player.transform.forward * 0.7f;
+        Vector3 spawnPosition = playerPosition + forward * 0.7f;
         spawnPosition.y += 0.1f;
 
         // 계산된 위치에 HairBall을 생성합니다.
         Instantiate(HairBall, spawnPosition, Quaternion.identity);
     }
 
-    bool CheckObstacleInFront()
+    public bool CheckObstacleInFront(Vector3 position, Vector3 forward)
     {
         // 플레이어 앞쪽 방향으로 레이캐스트를 수행하여 장애물을 확인합니다.
-        Vector3 playerPosition = Player.transform.position;
-        Vector3 playerForward = Player.transform.forward;
+        Vector3 playerPosition = position;
+        Vector3 playerForward = forward;
+        
         RaycastHit hit;
 
         if (Physics.Raycast(playerPosition, playerForward, out hit, obstacleCheckDistance))
--- Assets/Scripts/Item/Use/ProtectEnergyUse.cs
+++ Assets/Scripts/Item/Use/ProtectEnergyUse.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class ProtectEnergyUse : MonoBehaviour
 {
-    public GameObject Player;
     public GameObject ProtectEnergy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            CreateProtectEnergy();
-        }
-    }
-
-    void CreateProtectEnergy()
+    public void CreateProtectEnergy(Vector3 position, Transform transform)
     {
         {
             // 플레이어의 현재 위치에 오브젝트 생성
-            Vector3 playerPosition = Player.transform.position;
+            Vector3 playerPosition = position;
             playerPosition.y += 0.25f;
-            GameObject newObject = Instantiate(ProtectEnergy, playerPosition, Quaternion.identity, Player.transform);
+            GameObject newObject = Instantiate(ProtectEnergy, playerPosition, Quaternion.identity, transform);
 
             // 5초 뒤에 생성된 오브젝트 파괴
             Destroy(newObject, 5f);
