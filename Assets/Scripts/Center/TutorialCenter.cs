09fd064112e2e0e27ca645fe40e39bf22086fdb2
SeokBeomKo <enngo98@gmail.com>
2023-12-04 21:51:08 +0900

    Update : 튜토리얼 씬

--- Assets/Scripts/Center/TutorialCenter.cs
+++ Assets/Scripts/Center/TutorialCenter.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class TutorialCenter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("자막")]
+    [SerializeField] public Subtitle subtitle;
+    [SerializeField] public QuestSubtitle questSubtitle;
 }
