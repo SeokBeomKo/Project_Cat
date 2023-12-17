94da0aa16528addcf42e024fc4a243b5d1bfa070
mangruf <pinky0927@naver.com>
2023-12-17 12:42:27 +0900

    Feat : 로봇청소기 프로그레스바

--- Assets/Scripts/UI/03.Chase/MovementProgressBar.cs
+++ Assets/Scripts/UI/03.Chase/MovementProgressBar.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerMovementProgressBar : MonoBehaviour
+public class MovementProgressBar : MonoBehaviour
 {
-    public Transform player;
+    [Header("이동하는 물체")]
+    public Transform obj;
+
+    [Header("시작 위치")]
     public Transform startPoint;
+
+    [Header("끝 위치")]
     public Transform endPoint;
 
     public Slider progressBar;
@@ -15,7 +20,7 @@ public class PlayerMovementProgressBar : MonoBehaviour
     void Update()
     {
         float maxDistance = Vector3.Distance(new Vector3(startPoint.position.x, 0, startPoint.position.z) , new Vector3(endPoint.position.x, 0, endPoint.position.z));
-        float distance = Vector3.Distance(new Vector3(player.position.x, 0, player.position.z), new Vector3(endPoint.position.x, 0, endPoint.position.z));
+        float distance = Vector3.Distance(new Vector3(obj.position.x, 0, obj.position.z), new Vector3(endPoint.position.x, 0, endPoint.position.z));
         
         float progress = Mathf.Clamp01((maxDistance - distance) / maxDistance);
         progressBar.value = progress;
